6be983f708ba3073ec583d31cf9842f6d0c52011
Dominic Michael Silveira <dms24081999@gmail.com>
2020-03-15 21:30:03 +0530

    Added web server manual mode

--- part-2 (Two Way Comm)/access_point/access_point.ino
+++ part-2 (Two Way Comm)/access_point/access_point.ino
@@ -1,18 +1,21 @@
 #include <ESP8266WiFi.h>
-#include <ESPAsyncWebServer.h>
+#include <ESP8266WebServer.h>
 
 extern "C" {
   #include<user_interface.h>
 }
 
 String moisture="",temp="";
+String response="Don't water it!";
+bool manual=false;
 WiFiServer server(80);
-AsyncWebServer server1(8080);
+ESP8266WebServer server1(8080);
 const char* ssid = "delta2";
 const char* password = "24081999";
 IPAddress IP(192,168,4,15);
 IPAddress mask = (255, 255, 255, 0);
 byte ledPin = 2;
+
 void setup() {
  Serial.begin(9600);
  Serial.print("Setting soft-AP ... ");
@@ -27,6 +30,11 @@ void setup() {
  Serial.print("IP: "); Serial.println(WiFi.softAPIP());
  Serial.print("MAC:"); Serial.println(WiFi.softAPmacAddress());
   // Start server
+
+  server1.on("/", handle_OnConnect);
+  server1.on("/manualon", handle_manualon);
+  server1.on("/manualoff", handle_manualoff);
+  server1.onNotFound(handle_NotFound);
   server1.begin();
 }
 
@@ -34,33 +42,37 @@ void setup() {
 
 void loop() {
  delay(2000);
+ server1.handleClient();
+ 
  Serial.println("********************************");
+ Serial.println("Manual Mode = "+String(manual));
  client_status();
- Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum()); 
  WiFiClient client = server.available();
  if (!client) {return;}
  digitalWrite(ledPin, LOW);
  String request = client.readStringUntil('\r');
  Serial.println("From the station: " + request);
  client.flush();
- String response;
+ 
  moisture = getValue(request, ',', 0);
  temp = getValue(request, ',', 1);
- if(moisture=="DRY"){
-  response="Water it!";
- }else if(moisture=="WET"){
+ if(!manual){
+   if(moisture=="DRY"){
+    response="Water it!";
+   }else if(moisture=="WET"){
+    response="Don't water it!";
+   }else{
+    response="Invalid Request!";
+   }
+ }
+ if(manual){
   response="Don't water it!";
- }else{
-  response="Invalid Request!";
  }
  Serial.print("Byte sent to the station: ");
  Serial.println(client.println(response+"\r"));
  digitalWrite(ledPin, HIGH);
- server1.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain",moisture.c_str());
- });
+
  delay(2000);
- 
 }
 
 String getValue(String data, char separator, int index){
@@ -79,46 +91,74 @@ String getValue(String data, char separator, int index){
 }
 
 void client_status() {
- 
   unsigned char number_client;
   struct station_info *stat_info;
-  
   struct ip_addr *IPaddress;
   IPAddress address;
   int i=1;
-  
   number_client= wifi_softap_get_station_num();
   stat_info = wifi_softap_get_station_info();
-  
-  Serial.print(" Total Connected Clients are = ");
+  Serial.print("Total Connected Clients are = ");
   Serial.println(number_client);
-  
     while (stat_info != NULL) {
-    
       ipv4_addr *IPaddress = &stat_info->ip;
       address = IPaddress->addr;
-      
-      Serial.print("client= ");
-      
+      Serial.print("client = ");
       Serial.print(i);
       Serial.print(" IP adress is = ");
       Serial.print((address));
       Serial.print(" with MAC adress is = ");
-      String str = "Station #";
+      String str = "";
       for(i=0;i<6;i++){
         String val=String(stat_info->bssid[i], HEX);
-        if(val.length()==1){
-          str +=0;
-        }
+        if(val.length()==1){str +=0;}
         str +=val;
-        if(i<5){
-          str += ":";
-        }
+        if(i<5){str += ":";}
       }
       str += "\r\n";
       Serial.print(str);
       stat_info = STAILQ_NEXT(stat_info, next);
       i++;
-      Serial.println();
     }
+}
+
+void handle_OnConnect() {
+  server1.send(200, "text/html", SendHTML(manual)); 
+}
+
+void handle_manualon() {
+  manual = true;
+  server1.sendHeader("Location", "/",true);
+  server1.send(302, "text/html", SendHTML(true)); 
+}
+
+void handle_manualoff() {
+  manual = false;
+  server1.sendHeader("Location", "/",true);
+  server1.send(302, "text/html", SendHTML(false)); 
+}
+
+void handle_NotFound(){
+  server1.sendHeader("Location", "/",true);
+  server1.send(302, "text/plain", SendHTML(manual));
+}
+
+String SendHTML(uint8_t  manual1){
+  String ptr = "<!DOCTYPE html>\n";
+  ptr +="<html>\n";
+  ptr +="<head>\n";
+  ptr +="<title>Manual Control</title>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>Manual Mode</h1>\n";
+  ptr +="<p>Click to switch <b>Manual Mode</b> on and off.</p>\n";
+  ptr +="<form method=\"get\">\n";
+  if(manual1)
+  ptr +="<input type=\"button\" value=\"manual OFF\" onclick=\"window.location.href='/manualoff'\">\n";
+  else
+  ptr +="<input type=\"button\" value=\"manual ON\" onclick=\"window.location.href='/manualon'\">\n";
+  ptr +="</form>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
 }
