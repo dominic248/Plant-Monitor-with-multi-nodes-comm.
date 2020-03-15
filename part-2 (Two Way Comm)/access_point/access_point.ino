d7ec55507c461740edd4427ac83a1613a5a4a10b
Dominic Michael Silveira <dms24081999@gmail.com>
2020-03-15 20:11:05 +0530

    Added mac address and web server

--- part-2 (Two Way Comm)/access_point/access_point.ino
+++ part-2 (Two Way Comm)/access_point/access_point.ino
@@ -1,5 +1,13 @@
 #include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+extern "C" {
+  #include<user_interface.h>
+}
+
+String moisture="",temp="";
 WiFiServer server(80);
+AsyncWebServer server1(8080);
 const char* ssid = "delta2";
 const char* password = "24081999";
 IPAddress IP(192,168,4,15);
@@ -18,10 +26,16 @@ void setup() {
  Serial.println("Server started.");
  Serial.print("IP: "); Serial.println(WiFi.softAPIP());
  Serial.print("MAC:"); Serial.println(WiFi.softAPmacAddress());
+  // Start server
+  server1.begin();
 }
+
+
+
 void loop() {
  delay(2000);
  Serial.println("********************************");
+ client_status();
  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum()); 
  WiFiClient client = server.available();
  if (!client) {return;}
@@ -30,8 +44,8 @@ void loop() {
  Serial.println("From the station: " + request);
  client.flush();
  String response;
- String moisture = getValue(request, ',', 0);
- String temp = getValue(request, ',', 1);
+ moisture = getValue(request, ',', 0);
+ temp = getValue(request, ',', 1);
  if(moisture=="DRY"){
   response="Water it!";
  }else if(moisture=="WET"){
@@ -42,7 +56,11 @@ void loop() {
  Serial.print("Byte sent to the station: ");
  Serial.println(client.println(response+"\r"));
  digitalWrite(ledPin, HIGH);
+ server1.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain",moisture.c_str());
+ });
  delay(2000);
+ 
 }
 
 String getValue(String data, char separator, int index){
@@ -58,4 +76,49 @@ String getValue(String data, char separator, int index){
         }
     }
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void client_status() {
+ 
+  unsigned char number_client;
+  struct station_info *stat_info;
+  
+  struct ip_addr *IPaddress;
+  IPAddress address;
+  int i=1;
+  
+  number_client= wifi_softap_get_station_num();
+  stat_info = wifi_softap_get_station_info();
+  
+  Serial.print(" Total Connected Clients are = ");
+  Serial.println(number_client);
+  
+    while (stat_info != NULL) {
+    
+      ipv4_addr *IPaddress = &stat_info->ip;
+      address = IPaddress->addr;
+      
+      Serial.print("client= ");
+      
+      Serial.print(i);
+      Serial.print(" IP adress is = ");
+      Serial.print((address));
+      Serial.print(" with MAC adress is = ");
+      String str = "Station #";
+      for(i=0;i<6;i++){
+        String val=String(stat_info->bssid[i], HEX);
+        if(val.length()==1){
+          str +=0;
+        }
+        str +=val;
+        if(i<5){
+          str += ":";
+        }
+      }
+      str += "\r\n";
+      Serial.print(str);
+      stat_info = STAILQ_NEXT(stat_info, next);
+      i++;
+      Serial.println();
+    }
 }
